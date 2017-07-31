df24897a0759f17602324fa518baa26abf765d52
Hugo Kawamata <hugokawamata@gmail.com>
2017-07-31 21:53:10 +1000

    Add url adaptability

--- models.py
+++ models.py
@@ -1,5 +1,21 @@
-
+from lxml import html
+import requests
+import urls
 
 def decode_message(message):
-    searchList = message.split("\n")
+    cardList = message.split("\n")
+    searchList = []
+    for card in cardList:
+        search = card.replace(",", "%2C")
+        search = search.replace(" ", "+")
+        search = search.replace("'", "%27s")
+        search = search.replace(":", "%3A")
+        search = search.replace("!", "%21")
+        search = search.replace("&", "%26")
+        searchList.append(search)
     return searchList
+
+def get_prices(cardList):
+    for card in cardList:
+        page = requests.get("http://www.cardkingdom.com/catalog/search?search=header&filter%5Bname%5D=" + card)
+        tree = html.fromstring(page.content)
